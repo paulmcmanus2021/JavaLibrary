b565b46970d9810a40f4a7a81c4eda47995f3f2e
Paul McManus <pmcmanus2021@gmail.com>
2020-01-28 16:34:24 +0000

    Passes book property test

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before(){
+        book = new Book("Animal Farm", "George Orwell", "Political Fiction");
+    }
+
+    @Test
+    public void bookHasTitle(){
+        assertEquals("Animal Farm", book.getTitle());
+    }
+
+    @Test
+    public void bookHasAuthor(){
+        assertEquals("George Orwell", book.getAuthor());
+    }
+
+    @Test
+    public void bookHasGenre(){
+        assertEquals("Political Fiction", book.getGenre());
+    }
 }
