21e2eaa189a33f78873a4dc807cf37a72976d8e6
Paul McManus <pmcmanus2021@gmail.com>
2020-01-28 21:12:40 +0000

    Trying Hashmaps

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
 
     private ArrayList<Book> libraryCollection;
     private int libraryCapacity;
+
+    private HashMap<String,Integer> booksByGenre = new HashMap<String,Integer>();
+
+    booksByGenre.put("Political Fiction", 1);
+    booksByGenre.put("Textbook", 1);
+    booksByGenre.put("Adventure Fiction", 1);
+    booksByGenre.put("True Crime", 2);
+
 
 
     public Library(ArrayList<Book> libraryCollection, int libraryCapacity) {
